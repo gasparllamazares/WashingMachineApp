9ea2f84aa8bdedb9b5f56c041c95e3728a8b4f08
Gaspar Llamazares <github@gaspi.es>
2024-09-08 18:33:57 +0200

    Updated URL (urls.py) to handle the occupied hours floor/<int:floor_id>/occupied_hours/

--- WashingMachineApp/reservations/urls.py
+++ WashingMachineApp/reservations/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import FloorNonAvailableSlotsView
 
 urlpatterns = [
-    path('floor/<int:floor_id>/free_hours/', FloorNonAvailableSlotsView.as_view(), name='floor_free_slots'),
+    path('floor/<int:floor_id>/occupied_hours/', FloorNonAvailableSlotsView.as_view(), name='floor_free_slots'),
     ]
